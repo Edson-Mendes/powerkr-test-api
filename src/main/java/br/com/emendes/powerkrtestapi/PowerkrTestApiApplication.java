9ad21cab13aecfdb38bd298312597d0502e540e3
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-05-05 18:06:47 -0300

    Primeiro commit

---
+++ src/main/java/br/com/emendes/powerkrtestapi/PowerkrTestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emendes.powerkrtestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerkrTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerkrTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emendes/powerkrtestapi/PowerkrTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emendes.powerkrtestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerkrTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
